03ef4363bebec24dcb6a9253502bc8c9621e40aa
dj258255 <dj258255@naver.com>
2025-06-02 08:55:44 +0900

    .
    .

---
+++ apps/backend-api/src/test/java/org/individual/live/livestreaming/LiveStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.individual.live.livestreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
