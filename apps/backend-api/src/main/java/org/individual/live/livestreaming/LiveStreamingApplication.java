02a0e52358f09f56c4135a25a102ed1d2694ca57
dj258255 <dj258255@naver.com>
2025-06-01 21:31:17 +0900

    feat : 프론트엔드 백엔드 등등 추가 , 모노레포 구성

---
+++ apps/backend-api/src/main/java/org/individual/live/livestreaming/LiveStreamingApplication.java
@@ -0,0 +1,13 @@
+package org.individual.live.livestreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveStreamingApplication.class, args);
+    }
+
+}
